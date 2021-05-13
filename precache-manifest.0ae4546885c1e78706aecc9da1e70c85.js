4411c0a4a38e905a198b8abcc42204b9aaefa0b4
Rohit Ranjan <51811511+Rohit4426@users.noreply.github.com>
2021-05-13 22:47:21 +0530

    Updates

---
+++ precache-manifest.0ae4546885c1e78706aecc9da1e70c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0475ecd5d96276a172bd0d48e5e962a",
+    "url": "/Classification-app/index.html"
+  },
+  {
+    "revision": "66fbea6af34f0f66d87e",
+    "url": "/Classification-app/static/css/main.dec3fd5e.chunk.css"
+  },
+  {
+    "revision": "a82fa490e332c9f96ca2",
+    "url": "/Classification-app/static/js/2.1ebd355f.chunk.js"
+  },
+  {
+    "revision": "d0d94b7b6a99e79fdfa36e9f01d4bccf",
+    "url": "/Classification-app/static/js/2.1ebd355f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66fbea6af34f0f66d87e",
+    "url": "/Classification-app/static/js/main.d8d3db52.chunk.js"
+  },
+  {
+    "revision": "64f5eb254eeb0a13dde3",
+    "url": "/Classification-app/static/js/runtime-main.44a8684e.js"
+  }
+]);
